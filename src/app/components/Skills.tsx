efe5be275faae8d3d8bba36d4c87d21e63ed7456
s1834 <s18324@protonmail.com>
2024-08-08 01:36:18 +0530

    made the layout of how the skills and about will look like (Still very raw)

---
+++ src/app/components/Skills.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+export default function Skill() {
+  return (
+    <div className="flex flex-col w-screen h-screen bg-blue-600 rounded-lg">
+      <div className="grid grid-rows-12 grid-flow-col h-5/6 gap-4 m-10">
+        <div className="row-span-6 col-span-6 bg-blue-500 rounded-lg">
+          SKILLS
+        </div>
+        <div className="row-span-4 col-span-3  bg-blue-500  rounded-lg">C</div>
+        <div className="row-span-2 col-span-4  bg-blue-500  rounded-lg">
+          C++
+        </div>
+        <div className="row-span-2 col-span-2 bg-blue-500 rounded-lg">HTML</div>
+        <div className="row-span-2 col-span-1  bg-blue-500  rounded-lg">
+          CSS
+        </div>
+        <div className="row-span-2 col-span-1  bg-blue-500  rounded-lg">
+          JavaScript
+        </div>
+        <div className="row-span-2 col-span-2 bg-blue-500 rounded-lg">Rust</div>
+        <div className="row-span-3 col-span-2  bg-blue-500  rounded-lg">
+          Java
+        </div>
+        <div className="row-span-1 col-span-1 bg-blue-500  rounded-lg">
+          TypeScript
+        </div>
+        <div className="row-span-3 col-span-2 bg-blue-500 rounded-lg">
+          TailwindCSS
+        </div>
+        <div className="row-span-3 col-span-2  bg-blue-500  rounded-lg">
+          Bootstrap
+        </div>
+        <div className="row-span-3 col-span-2 bg-blue-500 rounded-lg">
+          Flask
+        </div>
+        <div className="row-span-3 col-span-1  bg-blue-500  rounded-lg">
+          ExpressJS
+        </div>
+        <div className="row-span-5 col-span-2  bg-blue-500  rounded-lg">
+          NodeJS
+        </div>
+        <div className="row-span-4 col-span-2 bg-blue-500 rounded-lg">
+          ReactJS
+        </div>
+        <div className="row-span-1 col-span-2  bg-blue-500  rounded-lg">
+          NextJS
+        </div>
+        <div className="row-span-2 col-span-1  bg-blue-500  rounded-lg">
+          MongoDB
+        </div>
+        <div className="row-span-2 col-span-1 bg-blue-500 rounded-lg">SQL</div>
+        <div className="row-span-4 col-span-2  bg-blue-500  rounded-lg">
+          Linux
+        </div>
+        <div className="row-span-3 col-span-2 bg-blue-500  rounded-lg">AWS</div>
+        <div className="row-span-2 col-span-1 bg-blue-500 rounded-lg">Git</div>
+        <div className="row-span-3 col-span-2  bg-blue-500  rounded-lg">
+          More....
+        </div>
+        <div className="row-span-2 col-span-1  bg-blue-500  rounded-lg">
+          Figma
+        </div>
+      </div>
+    </div>
+  );
+}
